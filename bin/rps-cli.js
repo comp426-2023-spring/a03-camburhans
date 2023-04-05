46730480075658e1716747d1c4bcceac0800350a
Cam Burhans <cburhans@email.unc.edu>
2023-04-04 20:17:05 -0400

    Add the help text for cli -h

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -5,3 +5,19 @@ import minimist from "minimist";
 
 const args = minimist(process.argv);
 
+if (args.h || args.help) {
+    console.log(
+        `Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`
+    );
+    process.exit(0);
+}
