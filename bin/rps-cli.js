edfb188dcefcdac6300199eb88f9c4b55dffc6ad
Cam Burhans <cburhans@email.unc.edu>
2023-04-04 20:47:15 -0400

    Fix rps-cli

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,62 +1,37 @@
 #!/usr/bin/env node
 
-import { rps } from '../lib/rpsls.js';
 import minimist from "minimist";
 
-const args = minimist(process.argv);
+const args = minimist(process.argv.slice(2), {
+    alias: {
+        r: "rules"
+    }
+});
+
+import { RPS, help_RPS, rules_RPS } from "../lib/rpsls.js";
 
 if (args.h || args.help) {
-    console.log(
-        `Usage: node-rps [SHOT]
-        Play Rock Paper Scissors (RPS)
-        
-          -h, --help      display this help message and exit
-          -r, --rules     display the rules and exit
-        
-        Examples:
-          node-rps        Return JSON with single player RPS result.
-                          e.g. {"player":"rock"}
-          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                          e.g {"player":"rock","opponent":"scissors","result":"win"}`
-    );
-    process.exit(0);
-}
-
-if (args.r || args.rules) {
-    console.log(
-        `Rules for Rock Paper Scissors:
-        - Scissors CUTS Paper
-        - Paper COVERS Rock
-        - Rock CRUSHES Scissors`
-    );
-    process.exit(0);
-}
-
-if (args._.length > 3) {
-    console.error("Arguments out of range.");
-    console.log(
-        `
-        Usage: node-rps [SHOT]
-        Play Rock Paper Scissors (RPS)
-        
-          -h, --help      display this help message and exit
-          -r, --rules     display the rules and exit
-        
-        Examples:
-          node-rps        Return JSON with single player RPS result.
-                          e.g. {"player":"rock"}
-          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                          e.g {"player":"rock","opponent":"scissors","result":"win"}
-        
-        Rules for Rock Paper Scissors:
-        - Scissors CUTS Paper
-        - Paper COVERS Rock
-        - Rock CRUSHES Scissors                   
-        `
-    );
-    process.exit(0);
-}else if (args._.length <= 2) {
-  console.log(JSON.stringify(rps()))
+    help_RPS();
+    process.exit();
+} else if (args.r || args.rules) {
+    rules_RPS();
+    process.exit();
 } else {
-  console.log(JSON.stringify(rps(...args._.slice(2))))
+    var playerMove = args._[0];
+
+    if (!playerMove) {
+        var result = { "player": "rock" };
+        console.log(JSON.stringify(result));
+        process.exit();
+    }
+
+    playerMove = playerMove.toLowerCase();
+
+    var result = RPS(playerMove);
+    
+    if (!(typeof result == "undefined")) {
+        console.log(JSON.stringify(result));
+        process.exit();
+    }
+    process.exit();
 }
