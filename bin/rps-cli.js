309056cad64a65d65253e70e1222ada07bc5f763
Cam Burhans <cburhans@email.unc.edu>
2023-04-04 20:21:59 -0400

    Add logic for arguments when running node-rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -31,3 +31,32 @@ if (args.r || args.rules) {
     );
     process.exit(0);
 }
+
+if (args._.length > 3) {
+    console.error("Arguments out of range.");
+    console.log(
+        `
+        Usage: node-rps [SHOT]
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}
+        
+        Rules for Rock Paper Scissors:
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors                   
+        `
+    );
+    process.exit(0);
+}else if (args._.length <= 2) {
+  console.log(JSON.stringify(rps()))
+} else {
+  console.log(JSON.stringify(rps(...args._.slice(2))))
+}
