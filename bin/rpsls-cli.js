3ef3cf8580a159ac6eaced17e7a501dcc33b7535
Cam Burhans <cburhans@email.unc.edu>
2023-04-04 20:24:50 -0400

    Add minimist and -h cli message for rpsls

--- bin/rpsls-cli.js
+++ bin/rpsls-cli.js
@@ -2,3 +2,22 @@
 
 import { rpsls } from "../lib/rpsls.js";
 import minimist from "minimist";
+
+const args = minimist(process.argv);
+
+if (args.h || args.help) {
+    console.log(
+        `Usage: node-rpsls [SHOT]
+        Play the Lizard-Spock Expansion of Rock Paper Scissors (RPSLS)!
+        
+          -h, --help        display this help message and exit
+          -r, --rules       display the rules and exit
+        
+        Examples:
+          node-rpsls        Return JSON with single player RPSLS result.
+                            e.g. {"player":"rock"}
+          node-rpsls rock   Return JSON with results for RPSLS played against a simulated opponent.
+                            e.g {"player":"rock","opponent":"Spock","result":"lose"}`
+    );
+    process.exit(0);
+}
