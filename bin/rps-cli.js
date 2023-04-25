62392cf2586d51977ebc95e5a74fb5ab235fc655
Cam Burhans <cburhans@email.unc.edu>
2023-04-25 16:31:01 -0400

    Trying something new

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,34 +1,49 @@
 #!/usr/bin/env node
 
 import { rps } from "../lib/rpsls.js"
-import minimist from "minimist"
+import minimist from 'minimist';
 
-const args = minimist(process.argv)
-if (args.h || args.help) {
+var args = minimist(process.argv.slice(2));
+
+function print_help() {
     console.log(`Usage: node-rps [SHOT]
-Play Rock Paper Scissors (RPS)
-  -h, --help      display this help message and exit
-  -r, --rules     display the rules and exit
-Examples:
-  node-rps        Return JSON with single player RPS result.
-                  e.g. {"player":"rock"}
-  node-rps rock   Return JSON with results for RPS played against a simulated opponent.
-                  e.g {"player":"rock","opponent":"scissors","result":"win"}`)
-    process.exit()
+        Play Rock Paper Scissors (RPS)
+        
+          -h, --help      display this help message and exit
+          -r, --rules     display the rules and exit
+        
+        Examples:
+          node-rps        Return JSON with single player RPS result.
+                          e.g. {"player":"rock"}
+          node-rps rock   Return JSON with results for RPS played against a simulated opponent.
+                          e.g {"player":"rock","opponent":"scissors","result":"win"}`);
+}
+
+function print_rules() {
+    console.log(`Rules for Rock Paper Scissors:
+        - Scissors CUTS Paper
+        - Paper COVERS Rock
+        - Rock CRUSHES Scissors`);
+}
+
+if (args.h || args.help) {
+    print_help();
+    process.exit(0);
 }
 
 if (args.r || args.rules) {
-    console.log(`Rules for Rock Paper Scissors:
-  - Scissors CUTS Paper
-  - Paper COVERS Rock
-  - Rock CRUSHES Scissors`)
-    process.exit()
+    print_rules();
+    process.exit(0);
 }
 
-if (args._.length <= 2) {
-    console.log(JSON.stringify(rps()))
-} else if (args._.length > 3) {
-    console.error("ERROR: arguments out of range")
-} else {
-    console.log(JSON.stringify(rps(...args._.slice(2))))
+let player_shot = args._[0];
+
+try {
+    console.log(JSON.stringify(rps(player_shot)));
+    process.exit(0);
+}
+catch (e) {
+    print_help();
+    print_rules();
+    process.exit(0);
 }
